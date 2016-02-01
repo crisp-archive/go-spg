00812d20278a88e71ffa294217f56595489b66d2
zhangwanlong <zhangwanlong@baidu.com>
2016-02-01 15:32:59 +0800

    main logic success

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
     "flag"
     "fmt"
     "os"
+
+    "github.com/crispgm/go-spg/generator"
+    "github.com/crispgm/go-spg/page"
+    "github.com/crispgm/go-spg/variables"
 )
 
 const VERSION string = "1.0"
@@ -13,7 +17,7 @@ func main() {
     helpPtr := flag.Bool("help", false, "Help")
     versionPtr := flag.Bool("version", false, "Version Info")
     liveModePtr := flag.Bool("live", false, "Run in live-mode")
-    generateModePtr := flag.Bool("generate", false, "Generate Static Site")
+    generateModePtr := flag.Bool("generate", false, "Generate File")
     srcPathPtr := flag.String("src", currentPath, "Destination Path")
     dstPathPtr := flag.String("dst", currentPath, "Source Path")
 
@@ -56,6 +60,17 @@ func live(srcPathPtr *string) {
 }
 
 func generate(srcPathPtr *string, dstPathPtr *string) {
-    fmt.Println(*srcPathPtr)
-    fmt.Println(*dstPathPtr)
+    var jsonBlob = []byte(`{"Value": "Monotremata"}`)
+    vrb := variables.New(jsonBlob)
+
+    err, page := page.New("/home/users/zhangwanlong/project/go-spg/testing/favorite-things.md", "/home/users/zhangwanlong/project/go-spg/testing/favorite-things.html", vrb, generator.G_MDX)
+    if err != nil {
+        panic("Test NewMdx failed")
+    }
+    page.LoadTemplate()
+    err, _ = page.Generate()
+    if err != nil {
+        panic("Test NewMdx Generate failed")
+    }
+    page.Output()
 }
